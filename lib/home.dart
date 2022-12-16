22e3a3aa9c659ded8959e0779497c2a8a4f32f6e
winkydes <keithlam0110@gmail.com>
2022-12-16 11:55:07 +0800

    basic setup

---
+++ lib/home.dart
@@ -0,0 +1,87 @@
+import 'package:AniClock/tempholder.dart';
+import 'package:flutter/material.dart';
+
+class BaseHomePage extends StatefulWidget {
+  const BaseHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<BaseHomePage> createState() => _BaseHomePageState();
+}
+
+class _BaseHomePageState extends State<BaseHomePage> {
+  int selectedIndex = 0;
+
+  var widgetOptions = [
+    const HomePage(),
+    const InfoPage(),
+    const ResourcePage(),
+    const AccountPage(),
+  ];
+
+  BottomAppBar botBar() {
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 0;
+                  });
+                },
+                icon: const Icon(Icons.home)),
+          ),
+          Expanded(
+            child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 1;
+                  });
+                },
+                icon: const Icon(Icons.show_chart)),
+          ),
+          Expanded(child: Text('')),
+          Expanded(
+            child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 2;
+                  });
+                },
+                icon: const Icon(Icons.tab)),
+          ),
+          Expanded(
+            child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 3;
+                  });
+                },
+                icon: const Icon(Icons.person)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AniClock"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.calendar_today_outlined),
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      //floating action button position to center
+      bottomNavigationBar: botBar(),
+      body: Center(
+        child: widgetOptions.elementAt(selectedIndex),
+      ),
+    );
+  }
+}
---
+++ lib/tempholder.dart
@@ -0,0 +1,60 @@
+// file for storing basic templates for undeveloped pages, to be deleted
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const Text("Development in progress0"));
+  }
+}
+
+class InfoPage extends StatefulWidget {
+  const InfoPage({Key? key}) : super(key: key);
+
+  @override
+  State<InfoPage> createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const Text("Development in progress1"));
+  }
+}
+
+class ResourcePage extends StatefulWidget {
+  const ResourcePage({Key? key}) : super(key: key);
+
+  @override
+  State<ResourcePage> createState() => _ResourcePageState();
+}
+
+class _ResourcePageState extends State<ResourcePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const Text("Development in progress2"));
+  }
+}
+
+class AccountPage extends StatefulWidget {
+  const AccountPage({Key? key}) : super(key: key);
+
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const Text("Development in progress3"));
+  }
+}
+
+
